d309e904126f7c0acaff0d552d62bb08387f3fc4
benwe95 <14256@ecam.be>
2019-06-17 20:21:53 +0200

    Updated README file

--- train.py
+++ train.py
@@ -9,17 +9,19 @@ data = []
 true_values = []
 with open('../training_3/driving_log.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
+	# Extract the images from the paths given in the csv file en resize them to keep the minimum useful information
     for row in reader:
 	    image = np.asarray(Image.open(row[0]))
 	    image_resized = scipy.misc.imresize(image, [66, 200])
 	    data.append(image_resized)
+		# Retrieve the correct value of the angle of the steering wheel for this image
 	    true_values.append(row[3])
 
 data = np.asarray(data)
 true_values = np.asarray(true_values)
 print("data loaded")
 
-# select data to balance zero and non-zero
+# select data to balance zero and non-zero. Keep only 1000 inputs where the value of the steering wheel is equal to 0
 index_of_non_0 = np.where(true_values != '0')[0]
 index_of_0 = np.where(true_values == '0')[0][0:1000]
 index = np.array(list(index_of_0) + list(index_of_non_0))
